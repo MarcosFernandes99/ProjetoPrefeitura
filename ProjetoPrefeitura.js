42ce7b76df9b4a98f09d2374b10abc471eb57ef1
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-09-08 22:28:46 -0300

    Função para calcular média de filhos da população finalizada

--- ProjetoPrefeitura.js
+++ ProjetoPrefeitura.js
@@ -1,13 +1,25 @@
 let salario = [1200, 1500, 2000, 5000, 10000, 1800]
 let numeroFilhos = [1, 3, 2, 1, 2, 4]
 let contador = 6
-let media = 0
+
 
 console.log("A média do salário da população é:", "R$", CalcularMediaSalario())
+console.log("A média de filhos da população é:", CalcularMediaFilhos())
 
 function CalcularMediaSalario(){
+    let media = 0
     for(let i = 0; i < contador; i++){
         media = media + salario[i]
+    }
+        media = media / contador
+
+        return media.toFixed(2)
+}
+
+function CalcularMediaFilhos(){
+    let media = 0
+    for(let i = 0; i < contador; i++){
+        media = media + numeroFilhos[i]
     }
         media = media / contador
 
